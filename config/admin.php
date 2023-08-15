a2b067b2613d8cf50ed1b99fb41c112e16733531
automaticko <automaticko@gmail.com>
2023-08-15 16:39:27 -0300

    Create admin seeder and config

---
+++ config/admin.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'uuid'        => env('ADMIN_UUID', '4b6059d0-aefe-4693-aff6-b2fbbdf82a7c'),
+    'password'    => env('ADMIN_PASSWORD'),
+    'avatar_uuid' => env('ADMIN_AVATAR_UUID', '4b6059d0-aefe-4693-aff6-b2fbbdf82a7d'),
+];
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\File;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Request;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $avatarUuid = Config::get('admin.avatar_uuid');
+        $adminUuid  = Config::get('admin.uuid');
+
+        $avatar = File::firstOrCreate(['uuid' => $avatarUuid], [
+            'uuid' => $avatarUuid,
+            'name' => 'admin_avatar',
+            'path' => 'avatars/admin',
+            'size' => '65535',
+            'type' => 'image/jpeg',
+        ]);
+
+        User::updateOrCreate(['uuid' => $adminUuid], [
+            'avatar_id'  => $avatar->getKey(),
+            'uuid'       => $adminUuid,
+            'first_name' => Config::get('admin.first_name', ''),
+            'last_name'  => Config::get('admin.last_name', ''),
+            'email'      => $this->email(),
+            'password'   => Hash::make(Config::get('admin.password')),
+        ]);
+    }
+
+    private function email(): string
+    {
+        if ($email = Config::get('admin.email')) {
+            return $email;
+        }
+
+        $host = Request::instance()->getHost();
+
+        return "admin@{$host}";
+    }
+}
