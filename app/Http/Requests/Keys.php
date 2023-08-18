096901b18640dcea6ab7ecaee55d4cde1487c97b
automaticko <automaticko@gmail.com>
2023-08-18 14:17:40 -0300

    Create request key constants

---
+++ app/Http/Requests/Keys.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests;
+
+interface Keys
+{
+    public const  FIRST_NAME            = 'first_name';
+    public const  LAST_NAME             = 'last_name';
+    public const  EMAIL                 = 'email';
+    public const  PASSWORD              = 'password';
+    public const  PASSWORD_CONFIRMATION = 'password_confirmation';
+    public const  AVATAR                = 'avatar';
+    public const  ADDRESS               = 'address';
+    public const  PHONE_NUMBER          = 'phone_number';
+    public const  MARKETING             = 'marketing';
+}
